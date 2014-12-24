6c9f481a36b0f65bb119f23f9e8d32822980ff10
taywils <DemetriousWilson@gmail.com>
2014-12-23 18:40:57 -0800

    Initial Commit

---
+++ EightPuzzle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EightPuzzle
+//
+//  Created by Demetrious  Wilson on 12/23/14.
+//  Copyright (c) 2014 Demetrious  Wilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
